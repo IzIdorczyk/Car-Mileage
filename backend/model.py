e9338b19483803a0c91ba111f8ae27564a956ced
IzI <kapi97s@wp.pl>
2023-02-16 13:37:51 +0100

    CM-6 done

---
+++ backend/model.py
@@ -0,0 +1,12 @@
+from typing import Dict
+from pydantic import BaseModel
+
+class Mileage(BaseModel):
+    plate: str
+    year: int
+    month: int
+    mileage: int
+
+class Car(BaseModel):
+    model: str
+    plate: str
